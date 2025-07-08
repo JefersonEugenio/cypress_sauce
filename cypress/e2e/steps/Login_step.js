d6de321aca6ad0372f05042e42b8033a410d1e42
Jeferson Lopes Eugenio <jefersonlopeseugenio11@gmail.com>
2025-07-08 17:01:20 -0300

    Coloquei dados sensivel

--- cypress/e2e/steps/Login_step.js
+++ cypress/e2e/steps/Login_step.js
@@ -13,18 +13,39 @@ after(() => {
   cy.log('Finalizou o teste de login')
 })
 
+
 Given(/^eu acesse a pagina home da aplicacao$/, () => {
-    loginPage.acessarNavegador()
+  loginPage.acessarNavegador()
 });
 
-When(/^eu inserir meu email "([^"]*)" e minha senha "([^"]*)"$/, (user,pass) => {
-    loginPage.fillLogin(user, pass)
+When(/^eu inserir meu email e senha validos$/, () => {
+	const user = Cypress.env('user_name')
+  const password = Cypress.env('user_password')
+
+  loginPage.fillLogin(user, password)
 });
 
 When(/^clicar no botao entrar$/, () => {
-    loginPage.clickButton()
+  loginPage.clickButton()
 });
 
 Then(/^tenho meu acesso "([^"]*)"$/, (mensagem) => {
-    loginPage.validar(mensagem)
+  loginPage.validar(mensagem)
 });
+
+
+// Given(/^eu acesse a pagina home da aplicacao$/, () => {
+//     loginPage.acessarNavegador()
+// });
+
+// When(/^eu inserir meu email "([^"]*)" e minha senha "([^"]*)"$/, (user,pass) => {
+//     loginPage.fillLogin(user, pass)
+// });
+
+// When(/^clicar no botao entrar$/, () => {
+//     loginPage.clickButton()
+// });
+
+// Then(/^tenho meu acesso "([^"]*)"$/, (mensagem) => {
+//     loginPage.validar(mensagem)
+// });
