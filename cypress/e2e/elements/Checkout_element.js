34baf9099c45e5fdc4fa2aba734801c2d1d293bc
Jeferson Lopes Eugenio <jefersonlopeseugenio11@gmail.com>
2025-07-10 12:04:27 -0300

    feat: adicionei novos elementos a pagina no checkout

---
+++ cypress/e2e/elements/Checkout_element.js
@@ -0,0 +1,9 @@
+class CheckoutElement {
+
+    firstName = () => { return '#first-name'}
+    lastName = () => { return '#last-name'}
+    zipPostal = () => { return '#postal-code'}
+
+    continueButton = () => { return '#continue'}
+
+} export default CheckoutElement
