3618e43ff59898ffe6f36b34fbe6509facec64ff
Jeferson Lopes Eugenio <jefersonlopeseugenio11@gmail.com>
2025-07-10 15:34:50 -0300

    add: adicionei mais elementos texto mensagem e botao 'Back Home'

--- cypress/e2e/elements/Overview_element.js
+++ cypress/e2e/elements/Overview_element.js
@@ -2,5 +2,8 @@ class OverviewElement {
 
     finish = () => { return '#finish' }
     cancel = () => { return '#cancel' }
-    
+
+    textMessage = () => { return '.complete-header' }
+    backHome = () => { return '#back-to-products'}
+
 } export default OverviewElement
