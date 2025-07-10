ae58c38bd5930b06288b2af7bf4cd8eb4f13d10a
Jeferson Lopes Eugenio <jefersonlopeseugenio11@gmail.com>
2025-07-10 11:01:03 -0300

    add: adicionei metodo para iniciar checkout

--- cypress/e2e/steps/FluxoDeCompra_step.js
+++ cypress/e2e/steps/FluxoDeCompra_step.js
@@ -1,8 +1,10 @@
 /// <reference types="cypress" />
 
 import ProdutoPage from "../pages/Produto_page"
+import CarrinhoPage from "../pages/Carrinho_page"
 
 const produtoPage = new ProdutoPage
+const carrinhoPage = new CarrinhoPage
 
 before(() => {
   cy.clearCookies()
@@ -25,7 +27,7 @@ When(/^adiciono um produto "([^"]*)" ao carrinho e acesso o carrinho$/, (item) =
 });
 
 When(/^inicio o processo de checkout e preencho as informações do comprador$/, () => {
-	return true;
+	carrinhoPage.checkout()
 });
 
 Then(/^finalizo a compra com sucesso$/, () => {
