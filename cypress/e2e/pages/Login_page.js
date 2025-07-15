06ee703e1a96de3a4c5044e65fef11e07cfb329f
Jeferson Lopes Eugenio <jefersonlopeseugenio11@gmail.com>
2025-07-14 22:10:43 -0300

    add: adicionei metodos e import faker

--- cypress/e2e/pages/Login_page.js
+++ cypress/e2e/pages/Login_page.js
@@ -1,5 +1,6 @@
 /// <reference types="cypress" /> 
 
+import { faker } from '@faker-js/faker'
 import LoginElement from "../elements/Login_element"
 
 const loginElement = new LoginElement
@@ -7,13 +8,38 @@ const url = Cypress.config('baseUrl')
 
 class LoginPage {
 
+    aleatorio(numero) {
+        const length = Number(numero)
+        const name = faker.string.alpha({ length: length})
+        console.log('nome gerado: ', name)
+        this.username(name)
+    }
+
     acessarNavegador() {
         cy.visit(url)
     }
 
+    username(user) {
+        cy.get(loginElement.usernameInput())
+            .should('be.visible')
+            .type(user)
+            .should('have.value', user)
+    }
+
+    password(password) {
+        cy.get(loginElement.passwordInput())
+            .should('be.visible')
+            .type(password)
+    }
+
     fillLogin(user, pass) {
-        cy.get(loginElement.usernameInput()).should('be.visible').type(user).should('have.value', user)
-        cy.get(loginElement.passwordInput()).should('be.visible').type(pass)
+        cy.get(loginElement.usernameInput())
+            .should('be.visible')
+            .type(user)
+            .should('have.value', user)
+        cy.get(loginElement.passwordInput())
+            .should('be.visible')
+            .type(pass)
     }
     
     clickButton() {
