1cc619d77718cad431626ed47c9b5707cfb769cc
Jeferson Lopes Eugenio <jefersonlopeseugenio11@gmail.com>
2025-07-15 15:26:34 -0300

    add: adicionei metodos titulo e selecionar, modifiquei nome metodo

--- cypress/e2e/pages/Produto_page.js
+++ cypress/e2e/pages/Produto_page.js
@@ -15,10 +15,21 @@ class ProdutoPage {
             .click()
     }
 
-    acessarCarrinho() {
+    removerProduto(item) {
+        cy.get(produtoElement.produtos())
+            .contains(item)
+            .should('be.visible')
+            .parents('.inventory_item')
+            .find('button')
+            .click()
+    }
+
+    iconeCarrinho() {
         cy.get(produtoElement.quantidadeCarrinho())
             .should('have.text', 1)
+    }
 
+    acessarCarrinho() {
         cy.get(produtoElement.carrinho())
             .should('be.visible')
             .click()
@@ -27,6 +38,33 @@ class ProdutoPage {
     validarProdutos() {
         cy.get(produtoElement.produtos())
             .should('be.visible')
+    }
+
+    titleProduto() {
+        cy.get(produtoElement.titleProduto())
+            .should('be.visible')
+    }
+
+    verificarIconeCarrinhoValor(valor) {
+        if (valor > 0) {
+            cy.get('.shopping_cart_badge')
+            .should('be.visible')
+            .and('have.text', String(valor));
+        } else {
+            cy.get('.shopping_cart_badge')
+            .should('not.exist');
+        }
+    }
+
+    verificarMenuOrdenacao() {
+        cy.get(produtoElement.menuOrdenacao())
+            .should('be.visible')
+    }
+
+    selecionarMenuOrdenacao(value) {
+        cy.get(produtoElement.menuOrdenacao())
+            .should('be.visible')
+            .select(value)
     }
 
 } export default ProdutoPage
