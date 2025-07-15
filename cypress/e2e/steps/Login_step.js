5bd58de9299a83ae9b4d894b9803c8d2b9706373
Jeferson Lopes Eugenio <jefersonlopeseugenio11@gmail.com>
2025-07-14 22:15:42 -0300

    test: adicionei cenarios de login senha incorreto, email invalido, campo a senha em branco, exibida mensagem de erro

--- cypress/e2e/steps/Login_step.js
+++ cypress/e2e/steps/Login_step.js
@@ -25,10 +25,46 @@ When(/^eu inserir meu email e senha validos$/, () => {
   loginPage.fillLogin(user, password)
 });
 
+When(/^eu deixo os campos de email e senha em branco$/, () => {
+  console.log("Deixam os campos em branco")
+});
+
 When(/^clicar no botao entrar$/, () => {
   loginPage.clickButton()
 });
 
 Then(/^tenho meu acesso "([^"]*)"$/, (mensagem) => {
   loginPage.validar(mensagem)
+});
+
+When(/^eu inserir meu email invalido e senha valido$/, () => {
+  const password = Cypress.env('user_password')
+
+  loginPage.fillLogin("usuario_invalido", password)
+});
+
+When(/^eu inserir meu email e senha incorreta$/, () => {
+	const user = Cypress.env('user_name')
+
+  loginPage.fillLogin(user, "senha_errada")
+});
+
+When(/^eu deixo o campo a senha em branco$/, () => {
+	const user = Cypress.env('user_name')
+
+  loginPage.username(user)
+});
+
+Then(/^exibida mensagem de erro "([^"]*)"$/, (mensagens) => {
+  loginPage.validar(mensagens)
+});
+
+When(/^eu inserir "([^"]*)" caracteres aleatorio no campo username$/, (numero) => {
+  loginPage.aleatorio(numero)
+  const password = Cypress.env('user_password')
+  loginPage.password(password)
+});
+
+Then(/^exibida mensagem de erro indicando o limite de caracteres$/, () => {
+	return true;
 });
