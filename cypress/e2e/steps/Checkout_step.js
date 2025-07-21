2fb5e3005f24e3f8fd576e36c99a093ee1ef3f08
Jeferson Lopes Eugenio <jefersonlopeseugenio11@gmail.com>
2025-07-21 16:37:05 -0300

    test: usar faker para nome, sobrenome e CEP no step do checkout

--- cypress/e2e/steps/Checkout_step.js
+++ cypress/e2e/steps/Checkout_step.js
@@ -1,11 +1,15 @@
 /// <reference types="cypress" />
-
+import { faker } from '@faker-js/faker'
 import CheckoutPage from "../pages/Checkout_page";
 
 const checkoutPage = new CheckoutPage
 
 When(/^eu inserir primeiro nome "([^"]*)", sobrenome "([^"]*)" e CEP "([^"]*)"$/, (nome,sobrenome,postal) => {
-	checkoutPage.preencherDados(nome, sobrenome, postal)
+	const primeiroNome = nome === 'faker' ? faker.person.firstName() : nome;
+	const ultimoNome = sobrenome === 'faker' ? faker.person.lastName() : sobrenome;
+	const cep = postal === 'faker' ? faker.location.zipCode('#######') : postal
+
+	checkoutPage.preencherDados(primeiroNome, ultimoNome, cep)
 });
 
 Then(/^clicar no botao Continue$/, () => {
