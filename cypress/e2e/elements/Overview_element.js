90e19f0b6dfb304867a53915f1c0502d61ee7c13
Jeferson Lopes Eugenio <jefersonlopeseugenio11@gmail.com>
2025-07-21 15:18:47 -0300

    refactor: modificar nome variavel

--- cypress/e2e/elements/Overview_element.js
+++ cypress/e2e/elements/Overview_element.js
@@ -3,7 +3,7 @@ class OverviewElement {
     finish = () => { return '#finish' }
     cancel = () => { return '#cancel' }
 
-    textMessage = () => { return '.complete-header' }
+    title = () => { return '.title' }
     backHome = () => { return '#back-to-products'}
 
 } export default OverviewElement
