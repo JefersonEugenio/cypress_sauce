d1d0aa8e995a804d282f836ad7eb02d54ce4fa84
Jeferson Lopes Eugenio <jefersonlopeseugenio11@gmail.com>
2025-07-22 16:12:00 -0300

    add: adicionar seletores para produto, subtotal, taxa e total

--- cypress/e2e/elements/Overview_element.js
+++ cypress/e2e/elements/Overview_element.js
@@ -7,4 +7,9 @@ class OverviewElement {
     message = () => { return '.complete-header' }
     backHome = () => { return '#back-to-products'}
 
+    product = () => { return '.inventory_item_name'}
+    itemTotal = () => { return '.summary_subtotal_label'}
+    taxa = () => { return '.summary_tax_label'}
+    total = () => { return '.summary_total_label'}
+
 } export default OverviewElement
