888c7bc17f8d708cddbf01ea2cd0ffaaefdee32b
Jeferson Lopes Eugenio <jefersonlopeseugenio11@gmail.com>
2025-08-19 16:46:00 -0300

    feat: adicionar comando para selecionar produto na tela de produtos

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,6 +1,10 @@
 import LoginPage from "../e2e/pages/Login_page"
+import ProdutoPage from "../e2e/pages/Produto_page";
+import CarrinhoPage from "../e2e/pages/Carrinho_page";
 
 const loginPage = new LoginPage()
+const produtoPage = new ProdutoPage()
+const carrinhoPage = new CarrinhoPage()
 
 Cypress.Commands.add('acessarLogin', title => {
   const user = Cypress.env('user_name')
@@ -10,5 +14,13 @@ Cypress.Commands.add('acessarLogin', title => {
   loginPage.fillLogin(user, password)
   loginPage.clickButton()
   loginPage.validar(title)
-  
+})
+
+Cypress.Commands.add('selecionadoProduto', () => {
+  produtoPage.titleProduto('Products')
+  produtoPage.validarProdutos()
+  produtoPage.adicionarProduto('Sauce Labs Backpack')
+  produtoPage.verificarIconeCarrinhoValor(1)
+  produtoPage.acessarCarrinho()
+  carrinhoPage.checkout()
 })
