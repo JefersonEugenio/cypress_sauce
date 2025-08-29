f859e11d662d793631a7ff6ba5222d18c0fb6b66
Jeferson Lopes Eugenio <jefersonlopeseugenio11@gmail.com>
2025-08-29 11:11:36 -0300

    refactor: remover import carrinho não utilizados

--- cypress/e2e/elements/Carrinho_element.js
+++
@@ -1,9 +0,0 @@
-class CarrinhoElement {
-    
-    continueShopping = () => { return '#continue-shopping'}
-    checkout = () => { return '#checkout'}
-    produto = () => { return '.cart_item'}
-    remove = () => { return '.btn'}
-    quantidade = () => { return '.cart_quantity'}
-
-} export default CarrinhoElement
