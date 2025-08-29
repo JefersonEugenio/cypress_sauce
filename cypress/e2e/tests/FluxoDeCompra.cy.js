acc4d8c44bede48d611cb21c9aee5eb642e149c9
Jeferson Lopes Eugenio <jefersonlopeseugenio11@gmail.com>
2025-08-29 16:47:58 -0300

    refactor: remover import e usar commands

--- cypress/e2e/tests/FluxoDeCompra.cy.js
+++ cypress/e2e/tests/FluxoDeCompra.cy.js
@@ -1,17 +1,13 @@
 /// <reference types="cypress" />
 
-import Funcao from '../../support/funcao'
-
-const user = Cypress.env('user_name')
-const password = Cypress.env('user_password')
-
 describe('Fluxo de compra', () => {
   it('Compra com sucesso', () => {
-    cy.visit(Cypress.config('baseUrl'))
-    Funcao.fazLogin(user, password)
-    Funcao.selecionadoProduto('Sauce Labs Backpack')
-    Funcao.preencherInformacao()
-    Funcao.finalizadoProduto()
+    cy.sessionLogin()
+    cy.userLogin(Cypress.env('user_name'))
+    cy.adicionarProduto('Sauce Labs Backpack')
+    cy.carrinhoProduto()
+    cy.checkout()
+    cy.overview()
   })
 
 })
