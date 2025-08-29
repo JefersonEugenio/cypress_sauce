d4a88009a96f00658dd30bb32540db196b30f01f
Jeferson Lopes Eugenio <jefersonlopeseugenio11@gmail.com>
2025-08-29 16:45:48 -0300

    feat: adicionar command customizado para adicionar overview

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -31,11 +31,11 @@ Cypress.Commands.add('adicionarProduto', (product) => {
     cy.get('.shopping_cart_link').click()
 })
 
-Cypress.Commands.add('carrinhoProduto', (product) => {
+Cypress.Commands.add('carrinhoProduto', () => {
     cy.contains('.btn', 'Checkout').should('be.visible').click()
 })
 
-Cypress.Commands.add('checkout', (product) => {
+Cypress.Commands.add('checkout', () => {
     cy.get('#first-name').should('be.visible')
             .type(faker.person.firstName())
         cy.get('#last-name').should('be.visible')
@@ -43,4 +43,36 @@ Cypress.Commands.add('checkout', (product) => {
         cy.get('#postal-code').should('be.visible')
             .type(faker.location.zipCode('#######'))
         cy.get('#continue').should('be.visible').click()
+})
+
+Cypress.Commands.add('overview', () => {
+    cy.get('.inventory_item_name').should('be.visible')
+        cy.get('@guardado').then((textoGuardado) => {
+        cy.get('.summary_subtotal_label')
+            .should('be.visible')
+            .should('contain', textoGuardado);
+        });
+        cy.get('@guardado').then(textoGuardado => {
+            const valorItem = parseFloat(textoGuardado.replace(/[^0-9.,]/g, '').replace(',', '.')) || 0;
+
+            cy.get('.summary_tax_label')
+                .should('be.visible')
+                .invoke('text')
+                .then(textoTaxa => {
+                    const valorTaxa = parseFloat(textoTaxa.replace(/[^0-9.,]/g, '').replace(',', '.')) || 0;
+
+                    const soma = valorItem + valorTaxa;
+
+                    cy.get('.summary_total_label')
+                        .should('be.visible')
+                        .invoke('text')
+                        .then(textoTotal => {
+                            const valorTotal = parseFloat(textoTotal.replace(/[^0-9.,]/g, '').replace(',', '.')) || 0;
+
+                            expect(valorTotal).to.be.closeTo(soma, 0.01);
+                        });
+                });
+        });
+        cy.get('#finish').should('be.visible').click()
+        cy.get('#back-to-products').should('be.visible').click()
 })
