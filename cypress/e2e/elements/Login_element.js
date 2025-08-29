364244e7cf6304cd9b1211f19eae285e5066c1cc
Jeferson Lopes Eugenio <jefersonlopeseugenio11@gmail.com>
2025-08-29 10:23:03 -0300

    refactor: remover arquivos de login não utilizados

--- cypress/e2e/elements/Login_element.js
+++
@@ -1,7 +0,0 @@
-class LoginElement  {
-
-    usernameInput = () => { return '#user-name'}
-    passwordInput = () => { return '#password'}
-    loginButton = () => { return '#login-button'}
-    
-} export default LoginElement
