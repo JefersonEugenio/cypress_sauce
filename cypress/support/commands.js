efc5faaa253a47bb01af36b90c7daf98b2bcbc04
Jeferson Lopes Eugenio <jefersonlopeseugenio11@gmail.com>
2025-08-29 12:11:09 -0300

    feat: adicionar command customizado para adicionar carrinho e clicar checkout

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -7,3 +7,28 @@ Cypress.Commands.add('userLogin', (username) => {
     cy.get('#user-name').clear().type(username)
     cy.get('#login-button').click()
 })
+
+Cypress.Commands.add('adicionarProduto', (product) => {
+    cy.get('.inventory_item_name')
+    .contains(product)
+    .should('be.visible')
+    .parents('.inventory_item')
+    .as('produtoItem')
+    cy.get('@produtoItem')
+    .find('.inventory_item_price')
+    .invoke('text')
+    .then(texto => {
+        cy.wrap(texto.trim()).as('guardado')
+    })
+    
+    cy.get('@produtoItem')
+    .find('button')
+    .click()
+    
+    cy.get('.shopping_cart_badge').should('be.visible').and('have.text', 1)
+    cy.get('.shopping_cart_link').click()
+})
+
+Cypress.Commands.add('carrinhoProduto', (product) => {
+    cy.contains('.btn', 'Checkout').should('be.visible').click()
+})
